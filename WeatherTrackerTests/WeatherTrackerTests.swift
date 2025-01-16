bd1fa95a6d1a87cab9f5b0a1aac3ce6281c9bb01
Oleksii Salmin <salminalex2012@gmail.com>
2025-01-16 01:09:01 -0600

    chore: tests cleanup.

--- WeatherTrackerTests/WeatherTrackerTests.swift
+++ WeatherTrackerTests/WeatherTrackerTests.swift
@@ -1,16 +1,6 @@
-//
-//  WeatherTrackerTests.swift
-//  WeatherTrackerTests
-//
-//  Created by Oleksii Salmin on 15.01.2025.
-//
-
 import Testing
 
 struct WeatherTrackerTests {
-
-    @Test func example() async throws {
-        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
-    }
-
+  @Test
+  func example() async throws { }
 }
